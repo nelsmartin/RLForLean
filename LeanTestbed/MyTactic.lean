053ac0a37d93e386b3f2e9f3949e56cbf961910b
nelsmartin <nels@nelsmartin.com>
2025-09-15 12:50:48 -0700

    proved all non-classical TPIL prop logic theorems

--- LeanTestbed/MyTactic.lean
+++ LeanTestbed/MyTactic.lean
@@ -17,8 +17,10 @@ The tactic will show options related to:
 8. Proving goals of type Or with Or.inl or Or.inl
 -/
 
---dbg_trace f!"error: {← e.toMessageData.toString}\n"
-
+--  dbg_trace f!"error: {← e.toMessageData.toString}\n"
+-- Note: maybe want to focus on first goal when feeding info to RL so that it can focus on what it needs to atm
+-- Infoview chooser!
+-- Could do this procedurally
 elab "iff_intro_option" : tactic =>
   Lean.Elab.Tactic.withMainContext do
       let goal ← getMainGoal
@@ -29,7 +31,7 @@ elab "iff_intro_option" : tactic =>
           evalTactic (← `(tactic| apply Iff.intro))
           let newGoals ← getGoals
           dbg_trace f!"apply Iff.intro"
-          dbg_trace f!"------------"
+          dbg_trace f!"---------------"
           for goal in newGoals do
             dbg_trace f!"{ ← ppGoal goal}\n"
         | _ => pure ()
@@ -44,7 +46,7 @@ elab "and_intro_option" : tactic =>
           evalTactic (← `(tactic| apply And.intro))
           let newGoals ← getGoals
           dbg_trace f!"apply And.intro"
-          dbg_trace f!"------------"
+          dbg_trace f!"---------------"
           for goal in newGoals do
             dbg_trace f!"{ ← ppGoal goal}\n"
         | _ => pure ()
@@ -240,26 +242,290 @@ elab "so" : tactic =>
 variable (p q r : Prop)
 
 -- commutativity of ∧ and ∨
-example : p ∧ q ↔ q ∧ p := sorry
-example : p ∨ q ↔ q ∨ p := sorry
+example : p ∧ q ↔ q ∧ p := by
+  apply Iff.intro
+  intro h
+  apply And.intro
+  let h_1 := h.right
+  apply h_1
+  let h_1 := h.left
+  apply h_1
+  intro h
+  let h_1 := h.left
+  let h_2 := h.right
+  apply And.intro
+  apply h_2
+  apply h_1
+
+
+example : p ∨ q ↔ q ∨ p := by
+  apply Iff.intro
+  intro h
+  cases h with | inl h_1 | inr h_1
+  apply Or.inr
+  apply h_1
+  apply Or.inl
+  apply h_1
+  intro h
+  cases h with | inl h_1 | inr h_1
+  apply Or.inr
+  apply h_1
+  apply Or.inl
+  apply h_1
 
 -- associativity of ∧ and ∨
-example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
-example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := by
+  apply Iff.intro
+  intro h
+  let h_1 := h.left
+  let h_2 := h.right
+  let h_3 := h_1.left
+  apply And.intro
+  apply h_3
+  let h_4 := h_1.right
+  apply And.intro
+  apply h_4
+  apply h_2
+  intro h
+  let h_1 := h.left
+  let h_2 := h.right
+  let h_3 := h_2.left
+  let h_4 := h_2.right
+  apply And.intro
+  apply And.intro
+  apply h_1
+  apply h_3
+  apply h_4
+
+
+
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := by
+  apply Iff.intro
+  intro h
+  cases h with | inl h_1 | inr h_1
+  cases h_1 with | inl h | inr h
+  apply Or.inl
+  apply h
+  apply Or.inr
+  apply Or.inl
+  apply h
+  apply Or.inr
+  apply Or.inr
+  apply h_1
+  intro h
+  cases h with | inl h_1 | inr h_1
+  apply Or.inl
+  apply Or.inl
+  apply h_1
+  cases h_1 with | inl h | inr h
+  apply Or.inl
+  apply Or.inr
+  apply h
+  apply Or.inr
+  apply h
 
 -- distributivity
-example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
-example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  intro h
+  let h_1 := h.left
+  let h_2 := h.right
+  cases h_2 with | inl h_3 | inr h_3
+  apply Or.inl
+  apply And.intro
+  apply h_1
+  apply h_3
+  cases h_2 with | inl h_4 | inr h_4
+  apply Or.inl
+  apply And.intro
+  apply h_1
+  apply h_4
+  apply Or.inr
+  apply And.intro
+  apply h_1
+  apply h_3
+  intro h
+  cases h with | inl h_1 | inr h_1
+  let h := h_1.left
+  let h_2 := h_1.right
+  apply And.intro
+  apply h
+  apply Or.inl
+  apply h_2
+  let h := h_1.left
+  let h_2 := h_1.right
+  apply And.intro
+  apply h
+  apply Or.inr
+  apply h_2
+
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := by
+  apply Iff.intro
+  intro h
+  cases h with | inl h_1 | inr h_1
+  apply And.intro
+  apply Or.inl
+  apply h_1
+  apply Or.inl
+  apply h_1
+  let h := h_1.left
+  let h_2 := h_1.right
+  apply And.intro
+  apply Or.inr
+  apply h
+  apply Or.inr
+  apply h_2
+  intro h
+  let h_1 := h.left
+  let h_2 := h.right
+  cases h_1 with | inl h_3 | inr h_3
+  cases h_2 with | inl h_4 | inr h_4
+  apply Or.inl
+  apply h_3
+  apply Or.inl
+  apply h_3
+  cases h_2 with | inl h_4 | inr h_4
+  apply Or.inl
+  apply h_4
+  apply Or.inr
+  apply And.intro
+  apply h_3
+  apply h_4
 
 -- other properties
-example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
-example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
-example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬(p ∧ ¬p) := sorry
-example : p ∧ ¬q → ¬(p → q) := sorry
-example : ¬p → (p → q) := sorry
-example : (¬p ∨ q) → (p → q) := sorry
-example : p ∨ False ↔ p := sorry
-example : p ∧ False ↔ False := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+example : (p → (q → r)) ↔ (p ∧ q → r) := by
+  apply Iff.intro
+  intro h
+  intro h_1
+  let h_2 := h_1.left
+  let h_3 := h_1.right
+  apply h
+  apply h_2
+  apply h_3
+  intro h
+  intro h_1
+  intro h_2
+  apply h
+  apply And.intro
+  apply h_1
+  apply h_2
+
+
+
+
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := by
+  apply Iff.intro
+  intro h
+  apply And.intro
+  intro h_1
+  apply h
+  apply Or.inl
+  apply h_1
+  intro h_1
+  apply h
+  apply Or.inr
+  apply h_1
+  intro h
+  intro h_1
+  let h_2 := h.left
+  let h_3 := h.right
+  cases h_1 with | inl h_4 | inr h_4
+  apply h_2
+  apply h_4
+  apply h_3
+  apply h_4
+
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := by
+  apply Iff.intro
+  intro h
+  apply And.intro
+  intro h_1
+  apply h
+  apply Or.inl
+  apply h_1
+  intro h_1
+  apply h
+  apply Or.inr
+  apply h_1
+  intro h
+  intro h_1
+  cases h_1 with | inl h_2 | inr h_2
+  let h_1 := h.left
+  let h_3 := h.right
+  apply h_1
+  apply h_2
+  let h_1 := h.right
+  apply h_1
+  apply h_2
+
+example : ¬p ∨ ¬q → ¬(p ∧ q) := by
+  intro h
+  intro h_1
+  cases h with | inl h_2 | inr h_2
+  let h := h_1.left
+  apply h_2
+  apply h
+  let h := h_1.right
+  apply h_2
+  apply h
+
+example : ¬(p ∧ ¬p) :=  by
+  intro h
+  let h_1 := h.left
+  let h_2 := h.right
+  apply h_2
+  apply h_1
+
+example : p ∧ ¬q → ¬(p → q) := by
+  intro h
+  intro h_1
+  let h_2 := h.left
+  let h_3 := h.right
+  apply h_3
+  apply h_1
+  apply h_2
+
+example : ¬p → (p → q) := by
+  intro h
+  intro h_1
+  apply False.elim
+  apply h
+  apply h_1
+
+
+example : (¬p ∨ q) → (p → q) := by
+  intro h
+  intro h_1
+  cases h with | inl h_2 | inr h_2
+  apply False.elim
+  apply h_2
+  apply h_1
+  apply h_2
+
+example : p ∨ False ↔ p := by
+  apply Iff.intro
+  intro h
+  cases h with | inl h_1 | inr h_1
+  apply h_1
+  apply False.elim
+  apply h_1
+  intro h
+  apply Or.inl
+  apply h
+
+example : p ∧ False ↔ False := by
+  apply Iff.intro
+  intro h
+  let h_1 := h.right
+  apply h_1
+  intro h
+  apply False.elim
+  apply h
+
+example : (p → q) → (¬q → ¬p) := by
+  intro h
+  intro h_1
+  intro h_2
+  apply h_1
+  apply h
+  apply h_2
--- LeanTestbed/PracticeTheorems.lean
+++ LeanTestbed/PracticeTheorems.lean
@@ -2,9 +2,8 @@ variable (p q r : Prop)
 
 -- commutativity of ∧ and ∨
 example : p ∧ q ↔ q ∧ p := sorry
+example : p ∨ q ↔ q ∨ p := sorry
 
-example : p ∨ q ↔ q ∨ p := by
-  sorry
 -- associativity of ∧ and ∨
 example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
 example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
@@ -16,11 +15,7 @@ example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
 -- other properties
 example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
 example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
-example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := by
-  sorry
-
-
-
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
 example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
 example : ¬(p ∧ ¬p) := sorry
 example : p ∧ ¬q → ¬(p → q) := sorry
@@ -29,36 +24,3 @@ example : (¬p ∨ q) → (p → q) := sorry
 example : p ∨ False ↔ p := sorry
 example : p ∧ False ↔ False := sorry
 example : (p → q) → (¬q → ¬p) := sorry
-
-
-/-
-Note:
-
-example : ((p ∧ q) → r) → (p → r) ∧ (q → r) := by
-  apply Iff.intro
-  apply False.elim
-This fails but
-
--/
-
-example : (¬p ∨ q) → (p → q) := by
-intro h
-intro h_1
-cases h with | inl hnp | inr hq
-apply False.elim
-apply hnp
-apply h_1
-apply hq
-
-
-
-
-
-
-example : (p ∨ q) → (q ∨ p) := by
-  intro h
-  cases h with | inl hnp | inr hq
-  apply Or.inr
-  apply hnp
-  apply Or.inl
-  apply hq
